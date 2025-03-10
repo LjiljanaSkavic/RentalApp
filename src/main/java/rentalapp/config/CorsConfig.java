59a72868d02a608e6ad051c964f5f78c5e403d11
Ljiljana Skavic <ljiljana.skavic@hotmail.com>
2025-03-10 22:18:07 +0100

    Add cors config

---
+++ src/main/java/rentalapp/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package rentalapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
